699773fc60b8ec2df94b747f8ee477f881302f5b
gabsc89o <user138066@eu249.local>
2018-04-18 14:31:11 +0100

    Initial Commit

---
+++ Calculadora/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculadora
+//
+//  Created by user138066 on 4/18/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculadora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculadora
+//
+//  Created by user138066 on 4/18/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
